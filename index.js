a24051f4ae87de1448dc90f8268c1cd1716b5eda
Joe Ganobsik <jganobsik@gmail.com>
2019-03-20 01:51:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+const logDriverNames = function (array) {
+  array.forEach(function (person) {
+    console.log(person.name);
+  });
+};
+
+const logDriversByHometown = function (array, hometown) {
+  array.forEach(function (person) {
+    if (person.hometown === hometown){
+    console.log(person.name);
+    }
+  });
+};
+
+
